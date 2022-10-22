1e23c2c7182dc5693b2d414e7900a5809acea13f
Tarik Ibrahimovic <tarikibrahimovic2016@gmail.com>
2022-10-22 19:15:31 +0200

    Add project files.

---
+++ my-booksReal/Data/Models/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace my_booksReal.Data.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    var configuration = new ConfigurationBuilder()
+        //        .SetBasePath(Directory.GetCurrentDirectory())
+        //        .AddJsonFile("appsettings.json")
+        //        .Build();
+
+        //    var connectionString = configuration.GetConnectionString("DefaultConnectionString");
+        //    optionsBuilder.UseSqlServer(connectionString);
+        //}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=my-books-db;Integrated Security=True;Pooling=False");
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ my-booksReal/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using my_booksReal.Data;
+using my_booksReal.Data.Models;
+using my_booksReal.Data.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.GetConnectionString("DefaultConnectionString");
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v2", new OpenApiInfo { Title = "my-books_updated_title", Version = "v2" });
+});
+
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnectionString");
+//builder.Services.AddDbContext<AppDbContext>(x =>
+//{
+//    x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+//});
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+
+builder.Services.AddTransient<BooksService>();
+
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    //Configure DBCOntext with sql
+    //builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Conn))
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v2/swagger.json", "my_books_updated_title v2"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+AppDbInitializer.Seed(app);
+
+app.Run();
